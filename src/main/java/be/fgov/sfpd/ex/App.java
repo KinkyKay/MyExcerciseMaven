91fdd412298c9bd7f4a2ab32c2815e783bb1b46e
Kim Van de Casteele <kim.vandecasteele@sfpd.fgov.be>
2020-10-01 11:11:09 +0200

    Initial commit

---
+++ src/main/java/be/fgov/sfpd/ex/App.java
@@ -0,0 +1,13 @@
+package be.fgov.sfpd.ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello again World!" );
+    }
+}
